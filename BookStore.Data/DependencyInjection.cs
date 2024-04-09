d506328fe751b8b0a893104dbd807742e78a8152
aysenuraydin <109242816+aysenuraydin@users.noreply.github.com>
2024-04-09 15:34:08 +0300

    #2

---
+++ BookStore.Data/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookStore.Data;
+public static class DependencyInjection
+{
+
+
+}
+
+
+
